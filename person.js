c5703e34528798a6eaf2650aa3cb7e1b407256cd
UlyaSkobeeva <127785319+UlyaSkobeeva@users.noreply.github.com>
2024-03-17 16:12:32 +0300

    Add files via upload

---
+++ person.js
@@ -0,0 +1,64 @@
+const list = document.querySelector("#card__list");
+const filter = document.querySelector("#filter");
+let PERSONS = [];
+
+//поиск
+filter.addEventListener("input", (event) => {
+  const value = event.target.value.toLowerCase();
+  const filteredPersons = PERSONS.filter((person) => {
+    return (
+      person.name.toLowerCase().includes(value) ||
+      person.job.toLowerCase().includes(value) ||
+      person.mail.toLowerCase().includes(value) ||
+      person.number.toLowerCase().includes(value) ||
+      person.year.toLowerCase().includes(value)
+    );
+  });
+  render(filteredPersons);
+});
+
+//получить данные из json
+async function start() {
+  try {
+    const resp = await fetch("./people.json");
+    const data = await resp.json();
+    PERSONS = data;
+
+    render(data);
+  } catch (err) {}
+}
+
+function render(persons = []) {
+  if (persons.length === 0) {
+    list.innerHTML = "Сотрудники не найдены";
+  } else {
+    const html = persons.map(toHTML).join("");
+    list.innerHTML = html;
+  }
+}
+
+function toHTML(person) {
+  return `
+  
+    <div class="card__item">
+        <div class="card__img"><img
+        src=" ${person.img}" alt=""
+        class="img">
+        </div>
+        <div class="card__info">
+            <h3 class="FIO">${person.name}</h3>
+            <h4 class="job">Должность: ${person.job}</h4>
+            <h4 class="telephone">Номер для связи: ${person.number}</h4>
+            <h4 class="mail">Эл. почта: ${person.mail}</h4>
+            <h4 class="date">Дата рождения: ${person.year}</h4>
+        </div>
+    </div>               
+    `;
+}
+
+// let bir = "3/27/2020";
+
+// const dshh = new Date(bir);
+// console.log(dshh);
+
+start();
--- script.js
+++ script.js
@@ -1,4 +1,74 @@
+//бургер
 document.querySelector(".burger").addEventListener("click", function () {
   this.classList.toggle("active");
   document.querySelector(".header-menu").classList.toggle("open");
 });
+//
+//
+//
+const list = document.querySelector("#age__list");
+async function start() {
+  try {
+    const resp = await fetch("./people.json");
+    const data = await resp.json();
+    PERSONS = data;
+    render(data);
+    // console.log(data);
+  } catch (err) {}
+}
+
+function render(persons = []) {
+  if (persons.length === 0) {
+    list.innerHTML = "Сотрудники не найдены";
+  } else {
+    const html = persons.map(toHTML).join("");
+
+    list.innerHTML = html;
+  }
+}
+
+function toHTML(person) {
+  let birDay = new Date(person.birthday);
+  let birthDay = birDay.toLocaleDateString();
+
+  let now = new Date();
+  let nowDay = now.toLocaleDateString();
+
+  let nowDate = now.getDate(); //число текущей даты
+  let birDate = birDay.getDate(); //число даты рождения
+  let nowMonth = now.getMonth(); //число текущего месяца
+  let birMonth = birDay.getMonth(); //число месяца рождения
+
+  if (nowDate <= birDate && nowMonth === birMonth) {
+    return `
+  <li class="info-age__item">
+  <img src="${person.img}" alt=""
+      class="info-age__img">
+  <p class="info-age__text">${person.name}</p>
+  <p class="info-age__year">${birthDay}</p>
+</li>             
+    `;
+  }
+}
+
+//слайдер
+let offset = 0; //смещение от низ
+const sliderLine = document.querySelector(".info-age__list");
+
+document.querySelector(".next__btn").addEventListener("click", function () {
+  offset = offset + 320;
+  if (offset > 1280) {
+    offset = 0;
+  }
+  sliderLine.style.bottom = offset + "px";
+});
+
+document.querySelector(".early__btn").addEventListener("click", function () {
+  offset = offset - 320;
+  if (offset < 0) {
+    offset = 1280;
+  }
+  sliderLine.style.bottom = offset + "px";
+});
+
+start();
